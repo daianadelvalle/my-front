a219ea661317ad6b78a50d0d75b3621d39e80bae
Daiana Delvalle <52934488+daianadelvalle@users.noreply.github.com>
2020-12-11 18:33:11 -0300

    my first commit in React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import About from './Component/About/About';
+import Portfolio from './Component/Portfolio/Portfolio'
+
+function App() {
+  return (
+    <div className="App">
+      <About/>
+      <Portfolio/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/About/About.js
@@ -0,0 +1,29 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import './About.css';
+
+function About(){
+    const [information, setInformation] = useState({});
+    useEffect(() => {
+        axios.get('https://daianadelvalleportfolio.herokuapp.com/About')
+        .then(
+            res => setInformation(res.data[0])
+        )}, [setInformation])
+    return(<div className="About-card">
+    <div className="div-foto">
+        <img className="foto-perfil" alt="FotoPerfil" src={information.photo}></img>
+    </div>
+    <div className="Descripcion">
+        <h1>{information.name}</h1>
+        <h2>{information.profession}</h2>
+        <p>{information.about_me}</p>
+        <ul className="skills">
+            { information.skills && information.skills.map((skill, index) => 
+                <p className="skill">{skill}</p>
+            )}
+        </ul>
+    </div>
+</div>)
+};
+
+export default About;
